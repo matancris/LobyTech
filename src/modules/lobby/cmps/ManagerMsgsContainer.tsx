a6acdcf3191a679c2fdd8cf6ba795a128cc3fe10
Matan Crispel <matan.cris@gmail.com>
2024-12-08 14:19:30 +0200

    added AppIcon CMP & option to remove msgs

--- src/modules/lobby/cmps/ManagerMsgsContainer.tsx
+++ src/modules/lobby/cmps/ManagerMsgsContainer.tsx
@@ -4,6 +4,7 @@ import { useStore } from "../../../store/useStore"
 import { ManagerMsg } from "../../../types/Lobby"
 import { AppButton } from "../../common/cmps/AppButton"
 import { ManagerMsgsList } from "./ManagerMsgsList"
+import { AppDialog } from "@/modules/common/cmps/AppDialog"
 
 interface Props {
   managerMsgs: ManagerMsg[]  // Array of messages from the manager
@@ -11,21 +12,54 @@ interface Props {
 
 export const ManagerMsgsContainer = ({ managerMsgs }: Props) => {
   const [msgText, setMsgText] = useState('')
+  const [msgToRemove, setMsgToRemove] = useState<ManagerMsg | null>(null)
+  const [isRemoveDialogOpen, setIsRemoveDialogOpen] = useState(false)
   const isEditMode = useStore((state) => state.isEditMode)
   const addManagerMsg = useStore((state) => state.addManagerMsg)
+  const removeManagerMsg = useStore((state) => state.removeManagerMsg)
+
 
   const onSaveMsg = () => {
+    msgText.trim()
+    if (!msgText) return;  // Prevent adding an empty message
     addManagerMsg(msgText);
     setMsgText('');
   }
+
+  const onRemoveMsg = (msg: ManagerMsg) => {
+    setIsRemoveDialogOpen(true)
+    setMsgToRemove(msg)
+  }
+
+  const removeMsg = () => {
+    if (msgToRemove) {
+      removeManagerMsg(msgToRemove)
+    }
+    setIsRemoveDialogOpen(false)
+  }
+
+  const onCancelRemove = () => {
+    setIsRemoveDialogOpen(false)
+    setMsgToRemove(null)
+  }
+
   return (
     <section className="manager-msgs-container">
-      <h2>Committee Messages</h2>
-      <ManagerMsgsList managerMsgs={managerMsgs} isEditMode={isEditMode} />
+      <ManagerMsgsList managerMsgs={managerMsgs} isEditMode={isEditMode} onRemoveMsg={onRemoveMsg} />
       {isEditMode && <div className="edit-input-section">
-        <input type="text" onChange={(ev) => setMsgText(ev.target.value)} value={msgText}/>
-        <AppButton text="add" onClick={onSaveMsg} />
+        <input type="text" onChange={(ev) => setMsgText(ev.target.value)} value={msgText} />
+        <AppButton text="הוסף" onClick={onSaveMsg} isDisabled={!msgText} />
       </div>}
+      {isRemoveDialogOpen &&
+        <AppDialog onClose={onCancelRemove} >
+          <div className="remove-dialog-content">
+            <h1>האם אתה בטוח שאתה רוצה למחוק את ההודעה?</h1>
+            <div className="dialog-btn-container flex">
+              <AppButton text="מחק" onClick={removeMsg} />
+              <AppButton text="ביטול" type="secondary" onClick={onCancelRemove} />
+            </div>
+          </div>
+        </AppDialog>}
     </section>
   )
 }
--- src/modules/lobby/cmps/ManagerMsgsPreview.tsx
+++ src/modules/lobby/cmps/ManagerMsgsPreview.tsx
@@ -1,13 +1,21 @@
+import { AppIcon } from "@/modules/common/cmps/AppIcon";
 import { ManagerMsg } from "../../../types/Lobby"
 
 
 interface Props {
     msg: ManagerMsg;
+    isEditMode: boolean;
+    onRemoveMsg: (msg: ManagerMsg) => void; // Function to remove the message from the state array when the user clicks on the delete icon.
 }
-export const ManagerMsgsPreview = ({ msg }: Props) => {
+export const ManagerMsgsPreview = ({ msg, isEditMode, onRemoveMsg }: Props) => {
     return (
         <section className="manager-msgs-preview">
-            {msg.text}
+            <p>{msg.text}</p>
+            {isEditMode &&
+                <div className="remove-msg-container">
+                    <AppIcon iconName="delete" onClick={() => onRemoveMsg(msg)}/>
+                </div>
+            }
         </section>
     )
 }
