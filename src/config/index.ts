2f891af5276f182ed0767f457e8ccc4788cda6e3
Matan Crispel <matan.cris@gmail.com>
2025-01-08 18:01:44 +0200

    added .env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,15 @@ export const config = {
   mode: import.meta.env.MODE,
   isDev: import.meta.env.MODE === 'dev',
   apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/',
-  firebaseApiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  firebase: {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    databaseURL: import.meta.env.VITE_API_URL
+  },
   weatherApiKey: import.meta.env.VITE_WEATHER_API_KEY
 };
